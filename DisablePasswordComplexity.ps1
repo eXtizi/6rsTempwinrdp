8b27d07f4ac0180cadf60050e25f47a43d21cbbb
eXtizi <75202685+eXtizi@users.noreply.github.com>
2021-04-09 08:03:21 +0530

    Create DisablePasswordComplexity.ps1

---
+++ DisablePasswordComplexity.ps1
@@ -0,0 +1,5 @@
+
+secedit /export /cfg c:\secpol.cfg
+(gc C:\secpol.cfg).replace("PasswordComplexity = 1", "PasswordComplexity = 0") | Out-File C:\secpol.cfg
+secedit /configure /db c:\windows\security\local.sdb /cfg c:\secpol.cfg /areas SECURITYPOLICY
+rm -force c:\secpol.cfg -confirm:$false
